b358a9d1777d662d131dc43b9cb95a9d03576ac2
草野浩俊 <kfactor.95.t@gmail.com>
2020-05-28 02:11:24 +0900

    add nullable(false) test

--- src/schemas/schema/__tests__/nullable.test.ts
+++ src/schemas/schema/__tests__/nullable.test.ts
@@ -3,5 +3,6 @@ import {Schema} from "../Schema";
 describe('nullable() method', () => {
     it('should change conditions.nullable to arg value', () => {
         expect(new Schema().nullable(true)["conditions"]["nullable"]).toBe(true);
+        expect(new Schema().nullable(false)["conditions"]["nullable"]).toBe(false);
     });
 });
