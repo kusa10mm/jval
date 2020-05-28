2de226c1076c3debdbd588ee4497c54cf575b776
草野浩俊 <kfactor.95.t@gmail.com>
2020-05-29 00:22:11 +0900

    move validateOneOf unit test

--- src/schemas/schema/__tests__/validate.test.ts
+++ src/schemas/schema/__tests__/validate.test.ts
@@ -5,19 +5,3 @@ describe('validate() method with no conditions', () => {
         await expect(new Schema().validate('value')).resolves.toBe('value');
     });
 });
-
-describe('validate() method with conditions.', () => {
-    it('should throw Error', async () => {
-        const schema = new Schema().oneOf(['hello', 5, false, null, undefined]);
-        await expect(schema.validate(3)).rejects.toThrowError()
-    });
-
-    it('should return value', async () => {
-        await expect(new Schema().oneOf(['hello', 5]).validate('hello')).resolves.toBe('hello');
-    });
-
-    it('should return value', async () => {
-        const schema = new Schema().oneOf(['hello', 5, false, null, undefined]);
-        await expect(schema.validate(undefined)).resolves.toBe(undefined);
-    });
-});
---
+++ src/validator/__tests__/validateOneOf.test.ts
@@ -0,0 +1,28 @@
+import {Conditions} from "../../types/conditions";
+import {validateOneOf} from "../validateOneOf";
+
+describe('test validateOneOf() method', () => {
+    it('should not throw error when oneOf condition is not set', () => {
+        const value = 'hello';
+        const conditions: Conditions = {
+            required: false, nullable: true
+        };
+        expect(validateOneOf(value, conditions)).toBeUndefined();
+    });
+
+    it('should not throw error when the value is included in oneOf condition', () => {
+        const value = 'hello';
+        const conditions: Conditions = {
+            required: false, nullable: true, oneOf: ['hello', 3, false, null, undefined]
+        };
+        expect(validateOneOf(value, conditions)).toBeUndefined();
+    });
+
+    it('should throw error when the value is not included in oneOf condition', () => {
+        const value = 'world';
+        const conditions: Conditions = {
+            required: false, nullable: true, oneOf: ['hello', 3, false, null, undefined]
+        };
+        expect(() => validateOneOf(value, conditions)).toThrowError();
+    })
+});
