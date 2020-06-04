0f2feaec8ab19f0b19cce04f8e2612abcf6215d0
草野浩俊 <kfactor.95.t@gmail.com>
2020-06-05 00:48:50 +0900

    improve baseValidate readability

--- src/validator/__tests__/baseValidate.test.ts
+++ src/validator/__tests__/baseValidate.test.ts
@@ -12,9 +12,11 @@ jest.mock('../validateOneOf');
 describe('test baseValidate() function', () => {
    it('should call validators with value and conditions', () => {
        const conditions: Conditions = {required: false, nullable: true};
+       
        baseValidate('hello', conditions);
-       expect(mocked(validateRequired)).toBeCalledWith('hello', {required: false, nullable: true});
-       expect(mocked(validateNullable)).toBeCalledWith('hello', {required: false, nullable: true});
-       expect(mocked(validateOneOf)).toBeCalledWith('hello', {required: false, nullable: true});
+
+       expect(mocked(validateRequired)).toBeCalledWith('hello', conditions);
+       expect(mocked(validateNullable)).toBeCalledWith('hello', conditions);
+       expect(mocked(validateOneOf)).toBeCalledWith('hello', conditions);
     })
 });
